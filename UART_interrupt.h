02fac4aa36c160b75c80433f19cfa7f1cf0884f8
vinod1999 <vinodsasanka1999@gmail.com>
2021-11-28 00:54:27 +0530

    still in development

---
+++ UART_interrupt.h
@@ -0,0 +1,19 @@
+/***********************************************************************
+  
+            https://github.com/VinodLiyanage/GLASS-CLEANER-ROBOT
+  -------------------------------- (C) ---------------------------------
+  
+  @name UART_interrupt.h
+  @version 1.0.0
+  @author Vinod Liyanage
+  @description UART serial communication library (interrupt method)
+
+************************************************************************/
+
+
+#ifndef UART_INTERRUPT_H_
+#define UART_INTERRUPT_H_
+
+void UART_init(unsigned long F_CPU, unsigned long BAUDRATE);
+
+#endif /* UART_INTERRUPT_H_ */
