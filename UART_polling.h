57800517f971e35fb41b5381a158c41da3f871f1
vinod1999 <vinodsasanka1999@gmail.com>
2021-11-27 21:03:25 +0530

    fix a typo in the header file

--- UART_polling.h
+++ UART_polling.h
@@ -1,7 +1,7 @@
 #ifndef UART_H_
 #define UART_H_
 
-void UART_init(long F_CPU, long BAUDRATE);
+void UART_init(unsigned long F_CPU, unsigned long BAUDRATE);
 char UART_RxChar();
 void UART_TxChar(char ch);
 void UART_SendString(char *str);
